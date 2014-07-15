34000e7f8a1e009dd8e5b7660ff2d45450019c75
Prateek Sharma <prateek123.shm@gmail.com>
2014-07-15 23:11:07 +0530

    Function to check click added

--- src/chess/game/Board.java
+++ src/chess/game/Board.java
@@ -24,9 +24,14 @@ public class Board extends JPanel
 			{"wp.gif","wn.gif","wb.gif","wr.gif","wq.gif","wk.gif"}
 	};
 	static Image imageurl[][]=new Image[2][6];											//imageurl will store the location of each image
+	static boolean player=true,computer=false;
+	Chess chess;
+	int fromx,fromy,tox,toy;
 	
 	Board(Chess chess)
 	{		
+		this.chess=chess;
+		System.out.println(chess);
 		JPanel boardpanel=new JPanel();
 		boardpanel.setLayout(new GridLayout(10,10));
 		
@@ -46,7 +51,7 @@ public class Board extends JPanel
 				{
 					int piece=Board_Default.PIECE_DEFAULT[i][j];
 					square[i][j].setIcon(new ImageIcon(imageurl[color][piece]));
-					pieceinfo[i][j]=new Piece(images[color][piece],i,j);
+					pieceinfo[i][j]=new Piece(images[color][piece].substring(0, 2),i,j);
 				}
 				else
 				{
@@ -65,7 +70,44 @@ public class Board extends JPanel
 	//Execute this function every time a square is clicked.
 	public void selected(int x,int y)
 	{
-		System.out.println(pieceinfo[x][y].getPiece().substring(0, 2));
+		System.out.println("Square selected");
+		if(computer)
+		{
+			System.out.println("Computer's move");
+			chess.setStatus("Computer is moving.Please wait.");
+			return;
+		}
+		if(player)
+		{
+			String p=pieceinfo[x][y].getPiece();
+			if(p==null)
+			{
+				System.out.println("Black square clicked");
+				chess.setStatus("You clicked on a blank square");
+				return;
+			}
+			else if(p.charAt(0)=='b')
+			{
+				System.out.println("Opponent's square clicked");
+				chess.setStatus("You clicked on the opponent's piece");
+			}
+			else
+			{
+				fromx=x;
+				fromy=y;
+				player=false;
+				chess.setStatus("Choose the destination");
+				System.out.println("Choose the destination");
+				return;
+			}
+		}
+		//System.out.println(pieceinfo[x][y].getPiece().substring(0, 2));
+		
+		if(fromx==x && fromy==y)
+		{
+			chess.setStatus("You clicked on the same square");
+			System.out.println("Clicked on the same square");
+		}
 	}
 	
 	//createLabel function is used to make top and bottom 'a' to 'z' labels on the board
