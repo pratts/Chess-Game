ed3612f02cdce5286f606cd68ac224ced6648c92
Prateek Sharma <prateek123.shm@gmail.com>
2014-07-21 01:03:26 +0530

    Move implements comparable to compare objects

---
+++ src/chess/game/Move.java
@@ -0,0 +1,25 @@
+package chess.game;
+
+public class Move implements Comparable
+{
+	int from,to,capture;
+	
+	Move(int f,int t,int c)
+	{
+		from=f;
+		to=t;
+		capture=c;
+	}
+	
+	public String toString()
+	{
+		//System.out.println((from>>3)+(from&7)+(":")+(to>>3)+(to&7));
+		return (from+(":")+to);
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
