a3e6bda8a9f00187a6f6298f5b2c32c3bcc57ca7
Prateek Sharma <prateek123.shm@gmail.com>
2014-07-23 21:04:20 +0530

    Unique hashcode and move score included

--- src/chess/game/Move.java
+++ src/chess/game/Move.java
@@ -2,7 +2,7 @@ package chess.game;
 
 public class Move implements Comparable<Object>
 {
-	int from,to,capture;
+	int from,to,capture,score;
 	
 	Move(int f,int t,int c)
 	{
@@ -11,15 +11,57 @@ public class Move implements Comparable<Object>
 		capture=c;
 	}
 	
+	protected void setScore(int s)
+	{
+		score=s;
+	}
+	
+	protected int getScore()
+	{
+		return score;
+	}
+	
+	public int hashCode() {
+        return from + (to) ;
+    }
+	
+	public boolean equals(Object o)
+	{
+		Move m=(Move)o;
+		return (from==m.from && to==m.to && capture==m.capture);
+	}
+	
 	public String toString()
 	{
 		//System.out.println((from>>3)+(from&7)+(":")+(to>>3)+(to&7));
-		return (from+(":")+to);
+		return (from/8+"-"+from%8+(":")+to/8+"-"+to%8);
 	}
 
 	@Override
-	public int compareTo(Object o) {
-		// TODO Auto-generated method stub
+	public int compareTo(Object o) 
+	{
+		Move m=(Move)o;
+		int mscore=m.getScore();
+		
+		if(score>mscore)
+		{
+			return -1;
+		}
+		if(score<mscore)
+		{
+			return 1;
+		}
+		int hash=hashCode();
+		int mhash=m.hashCode();
+		if(hash>mhash)
+		{
+			return 1;
+		}
+		if(hash<mhash)
+		{
+			return -1;
+		}
+		
 		return 0;
 	}
 }
