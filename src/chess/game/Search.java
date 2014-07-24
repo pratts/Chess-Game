58868e5b458f34558369560089b90c8fc711c740
Prateek Sharma <prateek123.shm@gmail.com>
2014-07-24 22:15:32 +0530

    Simplified Alpha-Beta and quiesce used

--- src/chess/game/Search.java
+++ src/chess/game/Search.java
@@ -12,6 +12,7 @@ public class Search
 	Move pv[][]=new Move[32][32];
 	int ply=0;
 	int MAX_PLY = 32;
+    Move m=null;
 	
 	Search(Game_Board gb)
 	{
@@ -20,103 +21,113 @@ public class Search
 	
 	public Move getMove()
 	{
-		return pv[0][0];
+		//System.out.println(m);
+		return m;
 	}
 	public void findmove(int max)
 	{
+		//System.out.println("ply      nodes  score  pv");
 		for(int i=0;i<max;i++)
 		{
-			int x=search(-1000000,1000000,i);
-			
+			int x=search(-100000,100000,i);
 			if(x>=10000 || x<=-10000)
 			{
 				break;
 			}
 		}
 		
-		System.out.println("Nodes searched: " + nodes);
+		//System.out.println("Nodes searched: " + nodes);
         return;
 	}
 	
 	int search(int alpha,int beta,int depth)
 	{
-		if (depth == 0)
+		if(depth==0)
+		{
 			return quiesce(alpha,beta);
-
-		boolean check = board.inCheck(board.side);
-		if (check)
-			++depth;
-		
-		if(ply>=MAX_PLY-1)
-		{
-			return board.evaluate();
 		}
-
-		LinkedList<Move> validMoves = board.getMoves();
+		++nodes;
 		
-		//boolean foundMove = false;
-		Iterator<Move> i = validMoves.iterator();
-		while (i.hasNext()) 
+		if (ply >= MAX_PLY - 1)
+            return board.evaluate();
+		
+		LinkedList<Move> move=board.getMoves();
+		Iterator<Move> i=move.iterator();
+		
+		while(i.hasNext())
 		{
-			Move move = (Move) i.next();
-	        if (!board.makeMove(move))
-	            continue;
-	       // foundMove = true;
-	        ply++;
-	        int x = search(-beta, -alpha, depth - 1);
-	        board.undoMove(move);
-	        ply--;
-	        if (x > alpha) 
-	        {
-	            /* this move caused a cutoff, so increase the history
-	                value so it gets ordered high next time we can
-	                search it */
-	           
-	            if (x >= beta)
-	                return x;
-	            alpha = x;
-	            pv[ply][ply]=move;
-            }
-		}	
+			Move temp=(Move)i.next();
+			if(!board.makeMove(temp))
+				continue;
+			ply++;
+			int score=-search(-beta,-alpha,depth-1);
+			board.undoMove(temp);
+			ply--;
+			if(score>=beta)
+			{
+				return beta;
+			}
+			
+			if(score>alpha)
+			{
+				alpha=score;
+				m=temp;
+				//System.out.println(m);
+			}
+			
+			if(alpha>beta)
+			{
+				break;
+			}
+		}
 		return alpha;
 	}
 
 
 	int quiesce(int alpha, int beta)
 	{
-		++nodes;
 		if (ply >= MAX_PLY - 1)
-			return board.evaluate();
-	
-		int x = board.evaluate();
-		if (x >= beta)
-	            return beta;
-		if (x > alpha)
-	            alpha = x;
-	
-		LinkedList<Move> validCaptures = board.getMoves();
-			
-	    Iterator<Move> i = validCaptures.iterator();
-		while (i.hasNext()) 
-		{
-			Move m = (Move) i.next();
-	            if (!board.makeMove(m))
-	                continue;
-	            ply++;
-	            x = -quiesce(-beta, -alpha);
-	            board.undoMove(m);
-	            ply--;
-	            if (x > alpha) 
-	            {
-	                if (x >= beta)
-	                    return beta;
-	                alpha = x;
-	
-					/* update the PV */
-	                pv[ply][ply] = m;
-	            }
+            return board.evaluate();
 		
+		int x=board.evaluate();
+		++nodes;
+
+		if(x>=beta)
+		{
+			return beta;
 		}
+		if(x>alpha)
+		{
+			alpha=x;
+		}
+		
+		LinkedList<Move> move=board.getMoves();
+		Iterator<Move> i=move.iterator();
+	
+		while(i.hasNext())
+		{
+			Move temp=(Move)i.next();
+			board.makeMove(temp);
+			ply++;
+			int score=-quiesce(-beta,-alpha);
+			board.undoMove(temp);
+			ply--;
+			if(score>=beta)
+			{
+				return beta;
+			}
+			if(score>alpha)
+			{
+				alpha=score;
+				m=temp;
+				//System.out.println(m);
+			}
+			if(alpha>beta)
+			{
+				break;
+			}
+		}
+		
 		return alpha;
 	}
 }
