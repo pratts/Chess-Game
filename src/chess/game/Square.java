a42c7dfd8ae08da557cde2303576d8b41c1420db
Prateek Sharma <prateek123.shm@gmail.com>
2014-07-28 12:06:27 +0530

    Square.java file commented

--- src/chess/game/Square.java
+++ src/chess/game/Square.java
@@ -1,3 +1,9 @@
+/*
+ * Square.java
+ * This java file is used to store information about each square displayed on the chess board.
+ * It will help to created a black/white colored square on the board and notifies the engine 
+ * every time a square is clicked.
+ */
 package chess.game;
 
 import java.awt.Color;
@@ -13,10 +19,10 @@ import javax.swing.JPanel;
 public class Square extends JPanel implements MouseListener
 {
 	private static final long serialVersionUID = -7245853391098781354L;
-	int x,y;
-	JLabel label;
+	int x,y;																//x and y are coordinated on the board
+	JLabel label;															//label is used to contain the image/tag on a square
 	Board board;
-	boolean mousein=false;
+	boolean mousein=false;													//to find whether the cursor is inside a square or not
 	
 	Square(int x,int y,Board board)
 	{
@@ -25,6 +31,7 @@ public class Square extends JPanel implements MouseListener
 		this.board=board;
 		
 		label=new JLabel();
+
 		if (((x + y) % 2) == 0) 
         {
         	setBackground(Color.white);									//Set background as white of the board square
@@ -33,22 +40,32 @@ public class Square extends JPanel implements MouseListener
         {
         	setBackground(Color.gray);									//Set background as black of the board square
         }
+		//sets the dimension of the square on the board
         setPreferredSize(new Dimension(65, 65));
+        
+        //sets the dimension of the label on the square
         label.setPreferredSize(new Dimension(55, 55));
         add(label);
         addMouseListener(this);
 	}
 	
+	//setIcon method sets the icon for the label on the square
 	void setIcon(Icon i)
 	{
+		//sets label at the center of each square
 		label.setVerticalAlignment(JLabel.CENTER);
 		label.setHorizontalAlignment(JLabel.CENTER);
+		
+		//add the icon on the label
 		label.setIcon(i);
 	}
 	
+	//paint method will paint the boundaries of the square in which the cursor enters
 	public void paint(Graphics g)
 	{
 		super.paint(g);
+		
+		//if mouse is inside,paint blue otherwise no paint
 		if(mousein==true)
 		{
 			g.setColor(Color.blue);
@@ -69,12 +86,14 @@ public class Square extends JPanel implements MouseListener
 	public void mouseReleased(MouseEvent e) {
 	}
 
+	//mouseEntered is call each time the cursor enters inside a square on the board
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		mousein=true;
 		repaint();
 	}
 
+	//mouseExited is called each time the cursor exits a square
 	@Override
 	public void mouseExited(MouseEvent e) {
 		mousein=false;
