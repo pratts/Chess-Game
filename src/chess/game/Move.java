d616a8751ddc0f54f7a3a60565c1833384abd7a0
Prateek Sharma <prateek123.shm@gmail.com>
2014-07-28 17:34:16 +0530

    Move.java commented

--- src/chess/game/Move.java
+++ src/chess/game/Move.java
@@ -1,3 +1,16 @@
+/*
+ * Move.java
+ * The Move will store the information about the move made on the board.
+ * A standard move will be of the form (from,to,capture) where capture means
+ * the piece captured.Capture can take different values:
+ * PAWN	 	-	0
+ * KNIGHT	-	1
+ * ROOK		-	2
+ * BISHOP	-	3
+ * QUEEN	-	4
+ * KING		-	5
+ * NO CAPTURE	-	6
+ */
 package chess.game;
 
 public class Move implements Comparable<Object>
@@ -11,31 +24,38 @@ public class Move implements Comparable<Object>
 		capture=c;
 	}
 	
+	//set score sets the score for each move.s is equal to (1000000 + (piece[to] * 10) - piece[from])
+	//Otherwise it is equal to the piece captured.
 	protected void setScore(int s)
 	{
 		score=s;
 	}
 	
+	//getScore returns the score of the move
 	protected int getScore()
 	{
 		return score;
 	}
 	
+	//hashcode returns the unique code related to each move
 	public int hashCode() {
         return from + (to) ;
     }
 	
+	//equals method is used to equate two move objects
 	public boolean equals(Object o)
 	{
 		Move m=(Move)o;
 		return (from==m.from && to==m.to && capture==m.capture);
 	}
 	
+	//toString method is used every time the Move object is printed on console 
 	public String toString()
 	{
 		return (from/8+""+from%8+""+to/8+""+to%8);
 	}
 
+	
 	@Override
 	public int compareTo(Object o) 
 	{
