5146d938c26f46b09635fdfa0488e13e6fa48fb2
Prateek Sharma <prateek123.shm@gmail.com>
2014-07-28 18:07:24 +0530

    Search.java commented

--- src/chess/game/Search.java
+++ src/chess/game/Search.java
@@ -1,3 +1,7 @@
+/*
+ * Search.java
+ * To search the best move using a basic form of alpha beta pruning algorithm.
+ */
 package chess.game;
 
 import java.util.Iterator;
@@ -14,27 +18,36 @@ public class Search
 		board=b;
 	}
 	
+	//To search for the best move
 	String alphabeta(int alpha,int beta,int depth,String move, int side)
 	{
+		//generate all the legal moves at each recursion step
 		LinkedList<Move> moves=board.getMoves();
 		System.out.println("AB:"+move+":"+alpha+":"+beta);
+		
+		//if we have reached the node or there is no legal move
 		if(depth==0 || moves.size()==0)
 		{
 			String temp=move+(board.evaluate()*(2*side-1));
 			return temp;
 		}
 		
+		//if the number of times a move can be made reaches the maximum value
 		if (ply >= maxply - 1)
             return move+(board.evaluate()*(2*side-1));
 		
+		//increase the tree depth if king is in check 
 		boolean check = board.inCheck(board.side);
 		if (check)
 			++depth;
 				
+		//iterator to iterate through all the moves
 		Iterator<Move> i=moves.iterator();
 		while(i.hasNext())
 		{
 			Move temp=(Move)i.next();
+			
+			//make a move
 			if(!board.makeMove(temp))
 				continue;
 			ply++;
@@ -43,8 +56,10 @@ public class Search
 			board.undoMove(temp);
 			ply--;
 			
+			//if player's move
 			if (side==1) 
 			{
+				//if the calculated value is less than minimum upper bound
 		        if (value<=beta) 
 		        {
 		        	beta=value; 
@@ -56,6 +71,7 @@ public class Search
 		    } 
 			else 
 		    {
+				//if the calculated value is less than maximum lower bound
 		        if (value>alpha) 
 		        {
 		        	alpha=value; 
@@ -66,6 +82,7 @@ public class Search
 		       	}
 		    }
 		            
+			//If both the bounds for the null set
 			if (alpha>=beta) 
 			{
 		        if (side==1) 
@@ -79,10 +96,12 @@ public class Search
 		    }
 		}
 	            
+		//if min node,return beta value
 		if (side==1) 
 		{
 			return move+beta;
 		} 
+		//if max node return alpha value
 		else 
 		{
 			return move+alpha;
